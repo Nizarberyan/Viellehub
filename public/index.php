ab0de8fc03f1b958b0cdd7739b96eb1212a21084
Nizar Beriane <nizarberiane43@gmail.com>
2025-02-05 17:30:03 +0100

    Add PHPMailer integration to send emails using SMTP
    Updated `public/index.php` to integrate PHPMailer for email sending via SMTP. Adjusted `.gitignore` to include `.txt` files and ensure proper organization of ignored files.

--- public/index.php
+++ public/index.php
@@ -1,4 +1,50 @@
-<!DOCTYPE html>
+<?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require '../vendor/autoload.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.eu.mailgun.org';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'VeilleHub@nizarberiane.me';                     //SMTP username
+    $mail->Password   = 'Nizar2005nizar#';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('support@nizarberiane.me', 'Mailer');
+    $mail->addAddress('badrnewacc@gmail.com', 'Ana');     //Add a recipient
+
+    $mail->addReplyTo('nizarberiane5@nizarberiane.me', 'Information');
+
+
+    //Attachments
+
+//    $mail->addAttachment('testimage.png', 'terraria');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+   $mail->Body    = 'Click bach check: <a href="instagram.com" style="background-color: #1D4ED8; color: white; padding: 10px 20px; text-decoration: none; border-radius: 5px; display: inline-block;">Click here</a>';;
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+//    echo 'Message has been sent';
+} catch (Exception $e) {
+//    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+?><!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
